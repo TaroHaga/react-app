bb8ea5522c32f0753bb0ace46e0c7fccacf84a89
TaroHaga <pdpark.bb@gmail.com>
2022-07-23 22:47:51 +0900

    react

---
+++ src/components/ActionButton.jsx
@@ -0,0 +1,11 @@
+// ActionButton.jsx
+
+export const ActionButton = ({ text, action }) => {
+    return (
+      <>
+        <button type="button" onClick={action}>
+          {text}
+        </button>
+      </>
+    );
+  };
